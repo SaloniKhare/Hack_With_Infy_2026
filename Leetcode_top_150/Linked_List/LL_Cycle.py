df643ca95b71e5a6b50fcde665fd9977202c7f89
Saloni Khare <salonikhare21@gmail.com>
2026-03-27 23:02:41 +0530

    Update LL_Cycle.py

--- Leetcode_top_150/Linked_List/LL_Cycle.py
+++ Leetcode_top_150/Linked_List/LL_Cycle.py
@@ -117,27 +117,3 @@ class RecursiveSolution:
         return self.hasCycle(head.next, visited)
 
 
-# ------------------------------------------------------
-# Example
-# ------------------------------------------------------
-
-# Creating a cycle manually:
-# 3 -> 2 -> 0 -> -4
-#      ^         |
-#      |_________|
-
-node1 = ListNode(3)
-node2 = ListNode(2)
-node3 = ListNode(0)
-node4 = ListNode(-4)
-
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node2  # cycle here
-
-print("BruteForce:", BruteForceSolution().hasCycle(node1))
-print("Optimal:", OptimalSolution().hasCycle(node1))
-print("Modify:", ModifySolution().hasCycle(node1))
-print("Dict:", DictSolution().hasCycle(node1))
-print("Recursive:", RecursiveSolution().hasCycle(node1))
